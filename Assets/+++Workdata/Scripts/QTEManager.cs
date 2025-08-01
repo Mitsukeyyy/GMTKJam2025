01e83869a0536df94458f82f67209cdb5d6c9241
mic.wilhelm <michael.wilhelm@stud.uni-hannover.de>
2025-08-01 16:00:00 +0200

    replaced QTE Keys with Words
    TODO: Fix Egg QTE

--- Assets/+++Workdata/Scripts/QTEManager.cs
+++ Assets/+++Workdata/Scripts/QTEManager.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
@@ -10,19 +13,24 @@ public class QTEManager : MonoBehaviour
 {
     [SerializeField] PlayerController playerController;
 
-    private string[] possibleKeys = Enumerable.Range('a', 26)
-        .Select(c => ((char)c).ToString()).ToArray();
+    // private string[] possibleKeys = Enumerable.Range('a', 26)
+    //     .Select(c => ((char)c).ToString()).ToArray();
 
-    private string correctKey;
-    private string eggKey;
-    private string eggKey2;
+    private string word;
+    private string eggWord;
+    private string eggWord2;
     private bool QTEActive = false;
     private bool isEggActive = false;
+    private WebClient client = new WebClient();
+    private string greenColor = "#1fa842";
+    private Color redColor = new Color(186, 12, 12);
+    private bool eggWordDone = false;
+    private bool eggWord2Done = false;
 
     [SerializeField] TextMeshProUGUI QTEText;
     [SerializeField] TextMeshProUGUI EggText;
     [SerializeField] Image QTEContainer;
-    private int buttonCount = 0;
+    // private int buttonCount = 0;
     public float timer = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -34,6 +42,10 @@ public class QTEManager : MonoBehaviour
         StartCoroutine(Egg());
     }
 
+    string getWord(int max)
+    {
+        return client.DownloadString($"https://random-word-api.herokuapp.com/word?length={Random.Range(3, max)}").Split('"')[1];
+    }
     void setQTEActive(bool state)
     {
         QTEText.gameObject.SetActive(state);
@@ -53,39 +65,60 @@ public class QTEManager : MonoBehaviour
             isEggActive = true; 
             playerController.speed = 0f;
 
-            eggKey = possibleKeys[Random.Range(0, possibleKeys.Length)];
-            eggKey2 = possibleKeys[Random.Range(0, possibleKeys.Length)];
-            while (eggKey2.Equals(eggKey))
+            eggWord = getWord(9);
+            string tempWord = eggWord;
+
+            eggWord2 = getWord(9);
+            string tempWord2 = eggWord2;
+
+            while (eggWord2.Equals(eggWord))
             {
-                eggKey2 = possibleKeys[Random.Range(0, possibleKeys.Length)];
+                eggWord2 = getWord(9);
             }
 
-            EggText.text = $"Press: {eggKey.ToUpper()} & {eggKey2.ToUpper()}";
+            EggText.text = $"Type: {eggWord} & {eggWord2}";
             EggText.gameObject.SetActive(true);
-            Debug.Log(buttonCount);
-            
-            while (buttonCount < 20)
+            // Debug.Log(buttonCount);
+            int i = 0;
+            int j = 0;
+            int k = 6;
+            while (!eggWordDone && !eggWord2Done)
             {
-                if (Input.GetKeyDown(eggKey) && buttonCount % 2 == 0)
+                if (Input.GetKeyDown(eggWord[i].ToString()) && !eggWordDone)
                 {
-                    buttonCount++;
-                    EggText.text = eggKey2.ToUpper();
-                    Debug.Log(buttonCount);
+                    EggText.text = $"<color={greenColor}>Type: {tempWord.Substring(0, i + 1)}</color>{tempWord.Substring(i+1, tempWord.Length - (i + 1))} & {eggWord2}";
+                    i++;
+                    k++;
+
+                    if (i == eggWord.Length - 1)
+                    {
+                        i = 0;
+                        k += 3;
+                        eggWordDone = true;
+                    }
                     yield return new WaitForSeconds(0.01f);
                 }
-                else if (Input.GetKeyDown(eggKey2) && buttonCount % 2 == 1)
+                else if (Input.GetKeyDown(eggWord2[j].ToString()) && eggWordDone)
                 {
-                    buttonCount++;
-                    EggText.text = eggKey.ToUpper();
-                    Debug.Log(buttonCount);
+                    EggText.text = $"<color={greenColor}>Type: {eggWord} & {tempWord2.Substring(0, j + 1)}</color>{tempWord2.Substring(j+1, tempWord2.Length - (j+1))}";
+                    j++;
+                    k++;
+
+                    if (j == eggWord2.Length - 1)
+                    {
+                        eggWord2Done = true;
+                    }
                     yield return new WaitForSeconds(0.01f);
+
                 }
-    
-                yield return null;
+
+                yield return new WaitForSeconds(0.01f);
             }
             playerController.speed = 3.5f;
-            buttonCount = 0;
-            EggText.gameObject.SetActive(false); 
+            EggText.gameObject.SetActive(false);
+            EggText.color = redColor;
+            eggWordDone = false;
+            eggWord2Done = false;
             isEggActive = false;
         }
         
@@ -101,12 +134,14 @@ public class QTEManager : MonoBehaviour
             }
             yield return new WaitForSeconds(Random.Range(2f, 5f));
 
-            correctKey = possibleKeys[Random.Range(0, possibleKeys.Length)];
-            QTEText.text = correctKey.ToUpper();
+            word = getWord(8);
+            string tempWord = word;
+            QTEText.text = word;
             QTEActive = true;
             setQTEActive(true);
 
             timer = 0f;
+            int i = 0;
             while (QTEActive)
             {
                 if (isEggActive)
@@ -116,9 +151,11 @@ public class QTEManager : MonoBehaviour
                     break;
                 }
 
-                if (Input.GetKeyDown(correctKey))
+                if (Input.GetKeyDown(word[i].ToString()) && i == word.Length - 1)
                 {
-                    if (playerController.speed < 6f && !Mathf.Approximately(playerController.speed, 5) )
+                    QTEText.text = $"<color={greenColor}>{word}</color>";
+                    Debug.Log("Right");
+                    if (playerController.speed < 6f && !Mathf.Approximately(playerController.speed, 5))
                     {
                         playerController.speed = 6f;
                     }
@@ -127,29 +164,27 @@ public class QTEManager : MonoBehaviour
                         playerController.speed++;
                     }
 
-                    QTEText.color = Color.green;
-                    Debug.Log(correctKey);
-                    QTEText.text = correctKey.ToUpper();
                     yield return new WaitForSeconds(0.5f);
-                    QTEText.color = Color.white;
+                    QTEText.color = redColor;
                     setQTEActive(false);
                     QTEActive = false;
+
+                }
+                else if (Input.GetKeyDown(word[i].ToString()))
+                {
+                    QTEText.text = $"<color={greenColor}>{tempWord.Substring(0, i + 1)}</color>{tempWord.Substring(i+1, tempWord.Length - (i+1))}";
+                    i++;
                 }
 
-                foreach (string key in possibleKeys)
+                if (!Input.GetKeyDown(word[i].ToString()) && playerController.speed > 0 && !Input.GetKeyDown(KeyCode.Space))
                 {
-                    if (key != correctKey && Input.GetKeyDown(key) && playerController.speed > 0)
-                    {
-                        playerController.speed -= 1f;
-                        QTEText.color = Color.red;
-                        yield return new WaitForSeconds(0.2f);
-                        QTEText.color = Color.white;
-                        Debug.Log(key);
-                    }
+                    playerController.speed -= 1f;
+                    QTEText.color = Color.red;
+                    yield return new WaitForSeconds(0.2f);
                 }
-                
-                timer  += Time.deltaTime;
-                if (timer >= 3f)
+
+                timer += Time.deltaTime;
+                if (timer >= 5f)
                 {
                     if (playerController.speed > 0)
                     {
@@ -158,10 +193,9 @@ public class QTEManager : MonoBehaviour
                     }
 
                     timer = 0f;
-                    
+
                     QTEText.color = Color.red;
                     yield return new WaitForSeconds(0.2f);
-                    QTEText.color = Color.white;
                     setQTEActive(false);
                     QTEActive = false;
                     break;
